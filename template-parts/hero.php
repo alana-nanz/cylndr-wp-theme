9abd2b66b4fafe2d73be9423509ead645a86e490
alana-nanz <alana.nanz@cylndr.com>
2024-07-22 15:50:11 -0400

    remove copy from hero

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -13,11 +13,3 @@
         <source src="<?php echo get_theme_file_uri()?>/assets/video/reel-desktop.mp4" type="video/mp4">
     </video>
 </div>
-
-<!-- <div class="coming-soon-inner">      
-    <div class="header-wrap">
-      <h1>CYLNDR <span>IS KNOWN FOR</span></h1>
-			<h1>MODERN CREATIVE PRODUCTION</h1>
-			<h1><span>POWERED BY</span> DESIGN.</h1>
-    </div>     
-</div> -->
